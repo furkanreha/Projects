cff92f2c6300eccd684286aba3c8be4bfec2690d
furkanreha <tutasfurkanreha@gmail.com>
2021-02-20 21:37:33 +0300

    Codes Are Added

---
+++ C#-Socket Samples/Simple Social Media App/CS408_PROJECT_STEP1/CS408_PROJECT_STEP1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace CS408_PROJECT_STEP1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.buttonListen = new System.Windows.Forms.Button();
+            this.richTextBoxLogs = new System.Windows.Forms.RichTextBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PORT:";
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(97, 65);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(289, 22);
+            this.textBoxPort.TabIndex = 1;
+            // 
+            // buttonListen
+            // 
+            this.buttonListen.Location = new System.Drawing.Point(404, 65);
+            this.buttonListen.Name = "buttonListen";
+            this.buttonListen.Size = new System.Drawing.Size(75, 23);
+            this.buttonListen.TabIndex = 2;
+            this.buttonListen.Text = "listen";
+            this.buttonListen.UseVisualStyleBackColor = true;
+            this.buttonListen.Click += new System.EventHandler(this.buttonListen_Click);
+            // 
+            // richTextBoxLogs
+            // 
+            this.richTextBoxLogs.Location = new System.Drawing.Point(43, 138);
+            this.richTextBoxLogs.Name = "richTextBoxLogs";
+            this.richTextBoxLogs.ReadOnly = true;
+            this.richTextBoxLogs.Size = new System.Drawing.Size(436, 406);
+            this.richTextBoxLogs.TabIndex = 3;
+            this.richTextBoxLogs.Text = "";
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Enabled = false;
+            this.buttonClear.Location = new System.Drawing.Point(218, 550);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 30);
+            this.buttonClear.TabIndex = 4;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 590);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.richTextBoxLogs);
+            this.Controls.Add(this.buttonListen);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Button buttonListen;
+        private System.Windows.Forms.RichTextBox richTextBoxLogs;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
